41011f6ead240df22b4262938c0bdeb646cd4f49
mac <mac@macs-MacBook-Air.local>
2024-03-01 17:25:14 +0400

    updates

--- ezshopapp/admin.py
+++ ezshopapp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Shop, ShopAdmin, Sale, Employee, UserProfile, AdminProfile, Service, BusinessProfile, DayClosing, SalesByAdminItem, SaleByAdminService, Role, SaleItem, ExpenseType, ReceiptType, Bank, ReceiptTransaction, PaymentTransaction, BankDeposit, Product, EmployeeTransaction, DailySummary, SalesByAdminItem, SalesByStaffItemService
 
+admin.site.site_header = "Ezshop Admin"
+admin.site.site_title = "Ezshop Admin Portal"
+admin.site.index_title = "Welcome to Ezshop Mite Solution App"
+
 admin.site.register(Shop)
 admin.site.register(ShopAdmin)
 admin.site.register(Sale)
