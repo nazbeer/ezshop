c898a84cdbaba078551e29a877debd55f7c1106b
Nasbeer Ahammed <nasbeer@mitesolutions.com>
2024-03-07 16:27:13 +0400

    django admin and user fixing

--- ezshopapp/admin.py
+++ ezshopapp/admin.py
@@ -8,14 +8,19 @@ admin.site.site_header = "Ezshop Admin"
 admin.site.site_title = "Ezshop Admin Portal"
 admin.site.index_title = "Welcome to Ezshop Mite Solution App"
 
-admin.site.register(Shop)
 
+admin.site.register(Shop)
+# class Shop(admin.ModelAdmin):
+#     list_display = ('shop', 'license_number')
+#     search_fields = ['shop__name', 'email']
+#     list_filter = ['shop']
+   
 class ShopAdminAdmin(admin.ModelAdmin):
     list_display = ('shop', 'email')
     search_fields = ['shop__name', 'email', 'admin_user__username']
     list_filter = ['shop']
     raw_id_fields = ['admin_user']
-
+    #max_num = 1 
     # def get_admin_users(self, obj):
     #     return ", ".join([admin.user.username for admin in obj.admin_user.all()])
     # get_admin_users.short_description = 'Admin Users'
@@ -26,7 +31,8 @@ class ShopInline(admin.StackedInline):
     model = ShopAdmin
     can_delete = False
     verbose_name_plural = 'Shop Admins'
-
+    max_num = 1 
+    
 class CustomUserAdmin(UserAdmin):
     inlines = (ShopInline,)
 
