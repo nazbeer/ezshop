e0896f93cb5db2fbb415ab0ca2271d77ee07824e
amal-dev-01 <devmpamal@gmail.com>
2024-04-10 11:58:47 +0530

    products services lists

--- ezshopapp/appviews.py
+++ ezshopapp/appviews.py
@@ -496,18 +496,25 @@ class SalesReportAPIView(APIView):
     
 
 
-class ProductListView(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+class ProductListView(APIView):
+    def get(self,request,pk):
+        employee =get_object_or_404(Employee,pk=pk)
+        queryset = Product.objects.filter(business_profile=employee.business_profile_id)
+        serializer= ProductSerializer(queryset,many=True)
+        return Response(serializer.data)
+    
+
+class ServiceListView(APIView):
+    def get(self,request,pk):
+        employee =get_object_or_404(Employee,pk=pk)
+        queryset = Service.objects.filter(business_profile=employee.business_profile_id)
+        serializer= ServiceSerializer(queryset,many=True)
+        return Response(serializer.data)
+
 
 class ProductDetailsView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-
-class ServiceListView(generics.ListAPIView):
-    queryset = Service.objects.all()
-    serializer_class = ServiceSerializer
 
 
 class ServiceDetailsView(generics.RetrieveAPIView):
